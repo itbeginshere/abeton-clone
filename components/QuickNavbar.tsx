da1978929a5876d1897ab0b815b0e72d2ce39fb8
Mcburd <itbeginshere852@gmail.com>
2022-08-20 20:36:56 +0200

    unnecessary pass ref

--- components/QuickNavbar.tsx
+++ components/QuickNavbar.tsx
@@ -2,14 +2,14 @@ import Link from 'next/link';
 
 const QuickNavbar = () : React.ReactElement => {
     return (
-        <div className={'flex flex-row'}>
-            <Link href={'#'} passHref>
+        <div className={'flex flex-row sticky'}>
+            <Link href={'#'}>
                 <span className={'text-xs font-semibold text-custom-coral p-5'}>About</span>
             </Link>
-            <Link href={'#'} passHref>
+            <Link href={'#'}>
                 <span className={'text-xs font-semibold text-custom-text p-5'}>Jobs</span>
             </Link>
-            <Link href={'#'} passHref>
+            <Link href={'#'}>
                 <span className={'text-xs font-semibold text-custom-text p-5'}>Apprenticeships</span>
             </Link>
         </div>
